903507a6456026ee07720420f8f3f8e54d1f3916
tersickpan <sickfan21@gmail.com>
2025-06-16 16:02:46 +0800

    Delete media-json-editor-complete/js directory

--- media-json-editor-complete/js/state.js
+++
@@ -1,5 +0,0 @@
-export let jsonData = { pictures: {}, videos: {} };
-export let currentType = 'pictures';
-export let selectedBaseKey = '';
-export let selectedKey = '';
-export let githubToken = '';  // Set this directly in the code if running locally
--- media-json-editor-complete/js/ui-common.js
+++
@@ -1,4 +0,0 @@
-export function renderJSON(data) {
-  const output = document.getElementById('json-output');
-  if (output) output.textContent = JSON.stringify(data, null, 2);
-}
