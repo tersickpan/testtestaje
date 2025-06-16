4212bc439b670c579fb6ac4cf2298b4799a4f3ba
tersickpan <sickfan21@gmail.com>
2025-06-16 17:47:54 +0800

    changed repo path

--- backup-2/js/fetcher.js
+++ backup-2/js/fetcher.js
@@ -1,7 +1,6 @@
 import { jsonData } from './state.js';
 
 export async function fetchJSON(type) {
-  // const response = await fetch(`https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/${type}.json`);
-  const response = await fetch(`./../${type}.json`);
+  const response = await fetch(`https://raw.githubusercontent.com/tersickpan/testtestaje/refs/heads/main/${type}.json`);
   jsonData[type] = await response.json();
 }
