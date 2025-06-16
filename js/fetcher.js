c943f954da25671e182a40ba92f2394d31d23cc1
tersickpan <sickfan21@gmail.com>
2025-06-16 16:20:47 +0800

    Update fetcher.js to access local JSON in repo

--- js/fetcher.js
+++ js/fetcher.js
@@ -1,6 +1,7 @@
 import { jsonData } from './state.js';
 
 export async function fetchJSON(type) {
-  const response = await fetch(`https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/${type}.json`);
+  // const response = await fetch(`https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/${type}.json`);
+  const response = await fetch(`../${type}.json`);
   jsonData[type] = await response.json();
 }
