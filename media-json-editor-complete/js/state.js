b09ef5f73a6040b01eda3665cffb7623bd75cca0
tersickpan <sickfan21@gmail.com>
2025-06-16 15:52:00 +0800

    Uploaded testing file generated from ChatGPT

---
+++ media-json-editor-complete/js/state.js
@@ -0,0 +1,5 @@
+export let jsonData = { pictures: {}, videos: {} };
+export let currentType = 'pictures';
+export let selectedBaseKey = '';
+export let selectedKey = '';
+export let githubToken = '';  // Set this directly in the code if running locally
