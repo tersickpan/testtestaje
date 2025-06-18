0ff8b7df08cf8a570eb386d1e7b96cfe67f47df7
tersickpan <sickfan21@gmail.com>
2025-06-19 04:30:50 +0800

    Delete backup-2 directory

--- backup-2/js/fetcher.js
+++
@@ -1,6 +0,0 @@
-import { jsonData } from './state.js';
-
-export async function fetchJSON(type) {
-  const response = await fetch(`https://raw.githubusercontent.com/tersickpan/testtestaje/refs/heads/main/${type}.json`);
-  jsonData[type] = await response.json();
-}
--- backup-2/js/updater.js
+++
@@ -1,9 +0,0 @@
-import { jsonData, currentType } from './state.js';
-
-export function addOrUpdateEntry(key, url, volume = null) {
-  if (currentType === 'pictures') {
-    jsonData.pictures[key] = url;
-  } else if (currentType === 'videos') {
-    jsonData.videos[key] = volume !== null ? { url, volume } : { url };
-  }
-}
