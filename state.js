78df603e21500406b78cfb083b02b8a5e1ef6f15
tersickpan <sickfan21@gmail.com>
2025-06-19 14:23:57 +0800

    Delete state.js

--- state.js
+++
@@ -1,7 +0,0 @@
-let dataFetched = false;
-let jsonData = { pictures: {}, videos: {} };
-let currentPage = '';
-let currentType = '';
-let selectedBaseKey = '';
-let selectedKey = '';
-let githubToken = '';  // Set this directly in the code if running locally
